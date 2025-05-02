938235cf1c0a90bdb41131bfe5e50d70c8d9aac7
BrandonGarate177 <brandongarate177@gmail.com>
2025-05-02 15:28:33 -0700

    Commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import MainLayout from './layout/MainLayout';
+
+const App = () => {
+  return (
+    <Router>
+      <MainLayout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </MainLayout>
+    </Router>
+  );
+};
+
+export default App;
