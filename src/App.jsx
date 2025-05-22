4520acc54457f9bfefd0c17411660d466af56985
BrandonGarate177 <brandongarate177@gmail.com>
2025-05-22 16:03:14 -0700

    Repo Wipe.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import MainLayout from './layout/MainLayout';
-import './styles/figma.css';
-
+import Main from './pages/Main.jsx';
 const App = () => {
   return (
     <Router>
-      <MainLayout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </MainLayout>
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
     </Router>
   );
 };
---
+++ src/pages/Main.jsx
@@ -0,0 +1,14 @@
+import react from 'react';
+import TopBar from '../sections/TopBar.jsx';
+export default function Main() {
+    return (
+        // here is where we are going to display the sections of the website
+
+        <div>
+            <TopBar />
+            <h1>Main Page</h1>
+            <p>This is the main page of the application.</p>
+        </div>
+    );
+
+}
